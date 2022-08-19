30f830d7e8fd098f6223fc0f75a1c666354913fc
oguzkoroglu <oguz.koroglu@userware.onmicrosoft.com>
2022-08-19 05:49:31 +0300

    directory service and controller completed

--- DirectoryService/Controllers/DirectoryController.cs
+++ DirectoryService/Controllers/DirectoryController.cs
@@ -1,6 +1,7 @@
 using DirectoryService.Data;
 using DirectoryService.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DirectoryService.Controllers
 {
@@ -20,7 +21,53 @@ namespace DirectoryService.Controllers
         [HttpGet(Name = "GetPersons")]
         public IEnumerable<Person>? Get()
         {
-            return _context.Person?.AsEnumerable();
+            var retval = _context.Person?.Include(x=>x.CommunicationInfos).AsEnumerable();
+            return retval;
+        }
+
+        [HttpPost(Name = "AddPerson")]
+        public Person AddPerson(Person p)
+        {
+            _context.Person?.Add(p);
+            _context.SaveChanges();
+            return p;
+        }
+
+        [HttpPost(Name = "DeletePerson")]
+        public void DeletePerson(Guid id)
+        {
+            var person = _context.Person?.FirstOrDefault(x=>x.UUID == id);
+            if (person == null)
+                throw new ArgumentNullException("No such person");
+            _context.Person?.Remove(person);
+            _context.SaveChanges();
+        }
+
+
+        [HttpPost(Name = "AddCommInfoToPerson")]
+        public void AddCommInfoToPerson(Guid personId, CommunicationInfo commInfo)
+        {
+            var person = _context.Person?.FirstOrDefault(x=>x.UUID == personId);
+            if (person == null)
+                throw new ArgumentNullException("No such person");
+
+            if (person.CommunicationInfos == null)
+                person.CommunicationInfos = new List<CommunicationInfo>();
+
+            person.CommunicationInfos.Add(commInfo);
+
+            _context.SaveChanges();
+        } 
+
+        
+        [HttpPost(Name = "DeleteCommunicationInfo")]
+        public void DeleteCommunicationInfo(int commInfoId)
+        {
+            var commInfo = _context.ConnectionInfos?.FirstOrDefault(x=>x.Id == commInfoId);
+            if (commInfo == null)
+                throw new ArgumentNullException("No such commInfo");
+            _context.ConnectionInfos?.Remove(commInfo);
+            _context.SaveChanges();
         }
     }
 }
